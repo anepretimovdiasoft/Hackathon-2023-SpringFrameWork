c14aef065e54eb5cd509ddd3fc4da76dec0ff488
anepretimovdiasoft <94528503+anepretimovdiasoft@users.noreply.github.com>
2023-04-02 22:01:11 +0300

    add dependencies

---
+++ src/main/java/com/samsung/spring/App.java
@@ -0,0 +1,12 @@
+package com.samsung.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+}
