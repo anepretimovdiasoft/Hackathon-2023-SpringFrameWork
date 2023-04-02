d0ceff5151906c54fae0f9ec6e416c1f23726f35
anepretimovdiasoft <94528503+anepretimovdiasoft@users.noreply.github.com>
2023-04-02 22:28:55 +0300

    add domain and application.yml

--- src/main/java/com/samsung/spring/App.java
+++ src/main/java/com/samsung/spring/App.java
@@ -1,12 +1,22 @@
 package com.samsung.spring;
 
+import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class);
+
+        try {
+            //Консоль для визуализации бд в браузере
+            Console.main(args);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/com/samsung/spring/domain/User.java
@@ -0,0 +1,31 @@
+package com.samsung.spring.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+}
+
